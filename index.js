e2041c1efc7ed2e48bd968710bc962f4a1571490
SUD <alesstosc@gmail.com>
2020-12-06 17:56:27 +0100

    funziona?

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.JS! il server funziona nella porta: '+process.env.PORT);
-}).listen(process.env.PORT);
-console.log('Server running '+process.env.PORT);
